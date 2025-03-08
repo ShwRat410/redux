7df38babc5f33731437cc6786572b9749def301c
ShwRat410 <shwetaratnawat410@gmail.com>
2025-03-08 10:07:04 +0000

    Creating store

---
+++ src/store/index.js
@@ -0,0 +1,20 @@
+import {createStore} from 'redux'
+
+const counterReducerFunction = (state={counter:0},action) => {
+    if(action.type==="INCREMENT"){
+        return{
+            counter:state.counter+1
+        }
+    }
+    if(action.type==="DECREMENT"){
+        return{
+            counter:state.counter-1
+        }
+    }
+    return state;
+
+}
+
+const store = createStore(counterReducerFunction)
+
+export default store
